ae808b91739e1dde1590f44c0c6c2f3bb8bf1ef5
Ikenna <Ikenna.ogbo@hotmail.com>
2018-11-11 19:00:34 +0000

    Updates

---
+++ precache-manifest.e057f9072acf429afbe967242aa5bb72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba54d26f118928d461c70a7a22ebbbf2",
+    "url": "/Portfolio/static/media/Stellar-Logo.ba54d26f.jpg"
+  },
+  {
+    "revision": "154bb208efd58ef216ff98756ca90058",
+    "url": "/Portfolio/static/media/Gold-iPhone.154bb208.png"
+  },
+  {
+    "revision": "d7373bde6b4a3c5b62e2b163b1203544",
+    "url": "/Portfolio/static/media/Calculator.d7373bde.jpg"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "918302293edd6a8ad59d",
+    "url": "/Portfolio/static/js/main.91830229.chunk.js"
+  },
+  {
+    "revision": "236dc17c36e82d280106",
+    "url": "/Portfolio/static/js/1.236dc17c.chunk.js"
+  },
+  {
+    "revision": "918302293edd6a8ad59d",
+    "url": "/Portfolio/static/css/main.3b564c44.chunk.css"
+  },
+  {
+    "revision": "36c268d477553457531662ede58565a1",
+    "url": "/Portfolio/index.html"
+  }
+];
