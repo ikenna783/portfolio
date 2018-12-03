99dd0a67a2726cc86dec563beb474f165a932c6d
Ikenna <Ikenna.ogbo@hotmail.com>
2018-12-03 13:15:06 +0000

    nav changes

--- src/index.js
+++ src/index.js
@@ -109,7 +109,7 @@ class Work extends Component {
             <a href="https://ikenna783.github.io/WordCount/" target="blank"><img src={require('./img/pen-paper.png')} alt="" /></a><p>Word Counter</p>
           </div>
           <div className="a col-lg-3 col-md-6">
-            <a href="https://ikenna783.github.io/Calculator/" target="blank"><img src={require('./img/Calculator.jpg')} alt="" /></a><p>JavaScript Calculator</p>
+            <a href="https://ikenna783.github.io/Calculator/" target="blank"><img src={require('./img/Calculator.jpg')} alt="" /></a><p>Calculator</p>
           </div>
           <div className="a col-lg-3 col-md-6">
             <a href="https://ikenna783.github.io/Phonepage/" target="blank"><img src={require('./img/Gold-iPhone.png')} alt="" /></a><p>Phone Page</p>
