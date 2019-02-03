23c113d37de4df5bbc105883fa8f8ab0fdf75cf5
Ikenna <Ikenna.ogbo@hotmail.com>
2019-02-03 15:16:16 +0000

    improvements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -70,7 +70,7 @@ class Home extends Component {
           <img src="https://media.licdn.com/dms/image/C4D03AQGcZ380lHHLcg/profile-displayphoto-shrink_200_200/0?e=1549497600&v=beta&t=K6iu7cAHmHCDwMOQBsh8YvP1z7s3ErCJUfS78y2BfP0" alt="" className="profile-img"/>
         </div>
         <div className="container intro-heading text-center">
-          <p>Hi, my name is Ikenna and {"I'm"} a Front End Developer based in London.</p>
+          <p>Hello, my name is Ikenna and {"I'm"} a Front End Developer based in London.</p>
         </div>
       </div>
     )
@@ -82,15 +82,15 @@ class Skills extends Component {
     return (
       <div>
       <div className="container intro-heading text-center">
-        <p>As a Front End Developer, I have worked on various projects using the technologies listed below.</p>
+        <p>As a Front End Developer, I have worked on various projects and have a good understanding of HTML, CSS, Bootstrap, JavaScript, ReactJS & GIT.</p>
       </div>
       <div className="container skills-section text-center">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png" alt=""/>
         <img src="http://www.idlewilddigital.com/sites/default/files/css3.png" alt=""/>
         <img src="http://pluspng.com/img-png/javascript-vector-png-java-script-js-logo-format-ai-213.png" alt="" id="js-logo"/>
-        <img src="https://i1.wp.com/jfdi.info/wp-content/uploads/bootstrap.png?ssl=1" alt="" id="bootstrap-logo"/>
+        <img src="https://servoy.com/wp-content/uploads/icon-bootstrap.png" alt="" id="bootstrap-logo"/>
           <img src="https://www.essitco.com/wp-content/uploads/2017/05/react.png" alt=""/>
-        <img src="https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png" alt="" id="git-logo"/>
+        <img src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png" alt="" id="git-logo"/>
       </div>
       </div>
     )
@@ -102,7 +102,7 @@ class Work extends Component {
     return (
       <div>
         <div className="container intro-heading text-center">
-          <p>Here are some examples of my work.</p>
+          <p>Here are some examples of my work ranging from personal projects to professional work.</p>
         </div>
         <div className="container projects text-center">
           <div className="a col-lg-3 col-md-6">
@@ -120,9 +120,9 @@ class Work extends Component {
           <div className="a col-lg-3 col-md-6">
             <a href="https://portal.teacherhq.co.uk/" target="blank"><img src={require('./img/TeacherHQ.jpg')} alt=""/></a><p>TeacherHQ Website</p>
           </div>
-          {/*<div className="a col-lg-3 col-md-6">
+          <div className="a col-lg-3 col-md-6">
             <a href="https://www.investcloud.com/Membership/Apps/v4IcHomePage_WF_App.aspx?ReturnURL=%2f#!/w/v4ichomepagewfapp?s=v4homeholderapp" target="blank"><img src={require('./img/InvestCloud.jpg')} alt=""/></a><p>Portals</p>
-          </div>*/}
+          </div>
           {/*<div className="a col-lg-3 col-md-6">
             <a href="http://www.ikstellar.com" target="blank"><img src={require('./img/Stellar-Logo.jpg')} alt="" /></a><p>Blog</p>
           </div>*/}
@@ -145,7 +145,7 @@ class Contact extends Component {
         <div className="container contact-details">
           <p><i>Mobile</i>: {"07861073043"}</p>
           <p><i>Email</i>: {"Ikenna.ogbo@hotmail.com"}</p>
-          <p><i><a href="https://www.linkedin.com/in/ikenna-obichukwu-359b5bb6/" target="blank">LinkedIn</a></i></p>
+          <p id="linkedin-btn"><i><a href="https://www.linkedin.com/in/ikenna-obichukwu-359b5bb6/" target="blank">LinkedIn</a></i></p>
         </div>
       </div>
     )
