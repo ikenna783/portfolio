58408368677bce7783d44497a77eaf207c4b2b68
Ikenna <Ikenna.ogbo@hotmail.com>
2019-04-09 16:12:42 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './index.css';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -10,9 +10,7 @@ class App extends Component {
         <div>
         <Route path="/" component={Navigation} />
         <Route exact path="/" component={Home} />
-        <Route path="/skills" component={Skills} />
-        <Route path="/work" component={Work} />
-        <Route path="/contact" component={Contact} />
+        <Route path="/portfolio" component={Portfolio} />
         </div>
       </Router>
     )
@@ -34,9 +32,7 @@ class Navigation extends Component {
         <div className="container text-center navigation hidden-sm hidden-xs">
         <ul className="links">
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/skills">Skills</Link></li>
-          <li><Link to="/work">Work</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/portfolio">Portfolio</Link></li>
         </ul>
       </div>
       <div className="container hamburger-menu hidden-md hidden-lg hidden-xl">
@@ -49,9 +45,7 @@ class Navigation extends Component {
       <div className="container text-center navigation hidden-md hidden-lg hidden-xl">
         <ul id="mobile-links">
           <li><Link to="/" onClick={showMenu}>Home</Link></li>
-          <li><Link to="/skills" onClick={showMenu}>Skills</Link></li>
-          <li><Link to="/work" onClick={showMenu}>Work</Link></li>
-          <li><Link to="/contact" onClick={showMenu}>Contact</Link></li>
+          <li><Link to="/portfolio" onClick={showMenu}>Portfolio</Link></li>
         </ul>
       </div>
     </div>
@@ -63,89 +57,59 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <div className="container name text-center">
+        <div className="container-fluid intro-section">
           <h1>Ikenna Obichukwu</h1>
+          <div className="container text-center">
+            <img src="https://media.licdn.com/dms/image/C4D03AQGcZ380lHHLcg/profile-displayphoto-shrink_200_200/0?e=1555545600&v=beta&t=wr8w-3dMTR9h1b0Q3rYlgUlxMHskHPTyvFooR29P1-g" alt="" className="profile-img"/>
+          </div>
+          <div className="container intro-heading text-center">
+            <p>Hello, my name is Ikenna and {"I'm"} a Front End Developer based in London.</p>
+          </div>
         </div>
-        <div className="container text-center">
-          <img src="https://media.licdn.com/dms/image/C4D03AQGcZ380lHHLcg/profile-displayphoto-shrink_200_200/0?e=1549497600&v=beta&t=K6iu7cAHmHCDwMOQBsh8YvP1z7s3ErCJUfS78y2BfP0" alt="" className="profile-img"/>
-        </div>
-        <div className="container intro-heading text-center">
-          <p>Hello, my name is Ikenna and {"I'm"} a Front End Developer based in London.</p>
-        </div>
-      </div>
-    )
-  }
-};
-
-class Skills extends Component {
-  render() {
-    return (
-      <div>
-      <div className="container intro-heading text-center">
-        <p>As a Front End Developer, I have worked on various projects and have a good understanding of HTML, CSS, Bootstrap, JavaScript, ReactJS & GIT.</p>
-      </div>
-      <div className="container skills-section text-center">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png" alt=""/>
-        <img src="http://www.idlewilddigital.com/sites/default/files/css3.png" alt=""/>
-        <img src="http://pluspng.com/img-png/javascript-vector-png-java-script-js-logo-format-ai-213.png" alt="" id="js-logo"/>
-        <img src="https://servoy.com/wp-content/uploads/icon-bootstrap.png" alt="" id="bootstrap-logo"/>
+        <div className="container-fluid skills-section text-center">
+          <div className="container intro-heading text-center">
+            <p>As a Front End Developer, I have worked on a great number of projects in various industries and have a good understanding of HTML, CSS, Bootstrap, JavaScript, ReactJS & GIT.</p>
+          </div>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png" alt=""/>
+          <img src="http://www.idlewilddigital.com/sites/default/files/css3.png" alt=""/>
+          <img src="http://pluspng.com/img-png/javascript-vector-png-java-script-js-logo-format-ai-213.png" alt="" id="js-logo"/>
+          <img src="https://servoy.com/wp-content/uploads/icon-bootstrap.png" alt="" id="bootstrap-logo"/>
           <img src="https://www.essitco.com/wp-content/uploads/2017/05/react.png" alt=""/>
-        <img src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png" alt="" id="git-logo"/>
-      </div>
+          <img src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png" alt="" id="git-logo"/>
+        </div>
+        <div className="container-fluid contact-section text-center">
+          <div className="container contact-details">
+            <p><i>Mobile</i>: {"07861073043"}</p>
+            <p><i>Email</i>: {"ikenna.ogbo@hotmail.com"}</p>
+            <p id="linkedin-btn"><i><a href="https://www.linkedin.com/in/ikenna-obichukwu-359b5bb6/" target="blank">LinkedIn</a></i></p>
+          </div>
+        </div>
       </div>
     )
   }
 };
 
-class Work extends Component {
+class Portfolio extends Component {
   render() {
     return (
       <div>
         <div className="container intro-heading text-center">
           <p>Here are some examples of my work ranging from personal projects to professional work.</p>
         </div>
-        <div className="container projects text-center">
-          <div className="a col-lg-3 col-md-6">
-            <a href="https://ikenna783.github.io/WordCount/" target="blank"><img src={require('./img/Writing.jpg')} alt="" /></a><p>Word Counter</p>
-          </div>
-          {/*<div className="a col-lg-3 col-md-6">
-            <a href="https://ikenna783.github.io/Calculator/" target="blank"><img src={require('./img/Calculator.jpg')} alt="" /></a><p>Calculator</p>
-          </div>*/}
-          <div className="a col-lg-3 col-md-6">
-            <a href="https://ikenna783.github.io/Todolist/" target="blank"><img src={require('./img/pen-paper.png')} alt="" /></a><p>Todo List</p>
-          </div>
-          <div className="a col-lg-3 col-md-6">
-            <a href="https://ikenna783.github.io/Phonepage/" target="blank"><img src={require('./img/Gold-iPhone.png')} alt="" /></a><p>Phone Page</p>
-          </div>
-          <div className="a col-lg-3 col-md-6">
-            <a href="https://portal.teacherhq.co.uk/" target="blank"><img src={require('./img/TeacherHQ.jpg')} alt=""/></a><p>TeacherHQ Website</p>
-          </div>
-          <div className="a col-lg-3 col-md-6">
-            <a href="https://www.investcloud.com/Membership/Apps/v4IcHomePage_WF_App.aspx?ReturnURL=%2f#!/w/v4ichomepagewfapp?s=v4homeholderapp" target="blank"><img src={require('./img/InvestCloud.jpg')} alt=""/></a><p>Portals</p>
-          </div>
-          {/*<div className="a col-lg-3 col-md-6">
-            <a href="http://www.ikstellar.com" target="blank"><img src={require('./img/Stellar-Logo.jpg')} alt="" /></a><p>Blog</p>
-          </div>*/}
-          {/*<div className="a col-lg-3 col-md-6">
-            <img src={require('')} alt="" /><p>Astra</p>
-          </div>*/}
+        <div className="portfolio-sections">
+          <a href="https://ikenna783.github.io/Todolist/" target="blank"><img src={require('./img/TodoList-ScreenShot.png')} alt="" className="screen-shots"/></a>
         </div>
-      </div>
-    )
-  }
-};
-
-class Contact extends Component {
-  render() {
-    return (
-      <div>
-        <div className="container text-center">
-          <img src="https://media.licdn.com/dms/image/C4D03AQGcZ380lHHLcg/profile-displayphoto-shrink_200_200/0?e=1549497600&v=beta&t=K6iu7cAHmHCDwMOQBsh8YvP1z7s3ErCJUfS78y2BfP0" alt="" className="profile-img"/>
+        <div className="portfolio-sections">
+          <a href="https://ikenna783.github.io/Phonepage/" target="blank"><img src={require('./img/PhonePage-ScreenShot.png')} alt="" className="screen-shots"/></a>
         </div>
-        <div className="container contact-details">
-          <p><i>Mobile</i>: {"07861073043"}</p>
-          <p><i>Email</i>: {"Ikenna.ogbo@hotmail.com"}</p>
-          <p id="linkedin-btn"><i><a href="https://www.linkedin.com/in/ikenna-obichukwu-359b5bb6/" target="blank">LinkedIn</a></i></p>
+        <div className="portfolio-sections">
+          <a href="https://portal.teacherhq.co.uk/" target="blank"><img src={require('./img/TeacherHQ-ScreenShot.png')} alt="" className="screen-shots"/></a>
+        </div>
+        <div className="portfolio-sections">
+          <a href="https://www.investcloud.com" target="blank"><img src={require('./img/InvestCloud-ScreenShot.png')} alt="" className="screen-shots"/></a>
+        </div>
+        <div className="portfolio-sections">
+          <a href="http://fincode.co.uk/" target="blank"><img src={require('./img/FinCode-ScreenShot.png')} alt="" className="screen-shots"/></a>
         </div>
       </div>
     )
